4cd9ab5b46615e80582e78082cd90fbdb8ecd705
lee <175937217@qq.com>
2023-12-02 11:30:48 +0800

    更改

---
+++ src/views/Category/index.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>分类页面</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,8 @@
+<script  setup>
+</script>
+<template>
+  <div>我是home页面</div>
+</template>
+
+<style scoped >
+</style>
